423518d6f7f17d4487b84e4778372bfb5dd9c712
Gunjan2025 <98686252+Gunjan2025@users.noreply.github.com>
2025-01-11 14:41:43 +0530

    Add files via upload

---
+++ src/utils/fetchData.js
@@ -0,0 +1,39 @@
+export const exerciseOptions={
+    
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+          'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
+          
+        },
+      
+};
+
+export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    },
+  };
+  
+ 
+  
+  
+export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
+//   export const fetchData = async (url, options) => {
+//     const updatedOptions = {
+//         ...options,
+//         params: { limit: '1000' },
+//     };
+
+//     const res = await fetch(url, updatedOptions);
+//     const data = await res.json();
+
+//     return data;
+// };
